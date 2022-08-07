7ff5c44cc7522783e59a5d48ea88d7e736d38420
cohouseol <cohouseol@gmail.com>
2022-08-07 16:25:11 +0900

    init
    프로젝트 새 생성
    
    gradle
    
    spring data jpa
    spring web
    spring devy tool
    lombok
    h2
    mysql
    
    추후 사용할 그래들 추후 추가 예정 
    현재 빈껍데기만 생성

---
+++ src/main/java/com/board/boardsite/BoardSiteApplication.java
@@ -0,0 +1,13 @@
+package com.board.boardsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardSiteApplication.class, args);
+    }
+
+}
