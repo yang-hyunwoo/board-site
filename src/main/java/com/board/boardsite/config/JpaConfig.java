3c8314e9d9bf0207a9c35cc5814c13894e1b8a13
cohouseol <cohouseol@gmail.com>
2022-08-18 01:01:29 +0900

    auditorAware 수정
    auditorAware 를 수정시에 
    @LastModifiedBy 수정자를 변경하기 위해서 수정을 하였다.
    전에는 에러가 자주 나와 고치지 못했던 부분을 오늘 게시글 수정을 하면서 이부분을 확인 하였는데
    .map(TripUserPrincipal::) 부분에서 500에러가 나와서 TODO로 빼놨었다.
    그거를 수정 하여 커밋했다.

--- src/main/java/com/board/boardsite/config/JpaConfig.java
+++ src/main/java/com/board/boardsite/config/JpaConfig.java
@@ -14,19 +14,13 @@ import java.util.Optional;
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
-    //TODO : 수정 일어날때 확인 해봐야 함
     @Bean
     public AuditorAware<String> auditorAware() {
-//
-//        System.out.println("aa::::"+Optional.ofNullable(SecurityContextHolder.getContext())
-//                .map(SecurityContext::getAuthentication)
-//                .map(Authentication::getPrincipal));
-//
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (null == authentication || !authentication.isAuthenticated()) {
-            return () -> Optional.of("System");
-        }
-        TripUserPrincipal tripUser = (TripUserPrincipal) authentication.getPrincipal();
-        return () -> Optional.of(tripUser.name());
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .map(Authentication::getPrincipal)
+                .map(TripUserPrincipal.class::cast)
+                .map(TripUserPrincipal::email);
+
     }
 }
