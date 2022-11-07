38ff0184ff78824e1d0338acdb7ff1d02865094c
cohouseol <cohouseol@gmail.com>
2022-11-07 21:02:17 +0900

    limit 수정

--- src/main/java/com/board/boardsite/repository/querydsl/travel/TravelAgencyCustomRepositoryImpl.java
+++ src/main/java/com/board/boardsite/repository/querydsl/travel/TravelAgencyCustomRepositoryImpl.java
@@ -50,6 +50,7 @@ public class TravelAgencyCustomRepositoryImpl extends QuerydslRepositorySupport 
                 .where(travelAgency.deleted.eq(false))
                 .groupBy(travelAgency.id,attachFile.filePath)
                 .orderBy(NumberExpression.random().asc())
+                .limit(count)
                 .fetch();
     }
 
